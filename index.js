066db65ccddbf101ead90d3afbc45176d73c7536
Cindy Velasquez <ciindvlz@hotmail.com>
2023-04-02 15:10:51 -0400

    FilterM

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(names, string) {
+    return names.filter(names => names.toLowerCase().substring(0, string.length) === string.toLowerCase())
+}
+
+function matchName(drivers, argument){
+    return drivers.filter(drivers => {return drivers.name === argument})
+}
